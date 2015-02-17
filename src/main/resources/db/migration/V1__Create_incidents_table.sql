9500c379ed51c09ab9946216ee556776a548476d
Josh Durbin <durbinjo593@gmail.com>
2015-02-17 12:48:18 -0800

    Adding SQL changes

--- src/main/resources/db/migration/V1__Create_incidents_table.sql
+++ src/main/resources/db/migration/V1__Create_incidents_table.sql
@@ -1,5 +1,22 @@
 CREATE TABLE incident (
-  id       SERIAL PRIMARY KEY,
-  createAt TIMESTAMP,
-  description TEXT
+
+  id SERIAL PRIMARY KEY,
+  insertionDate TIMESTAMP,
+  latitude INTEGER,
+  longitude INTEGER,
+  eventDate TIMESTAMP,
+  event TEXT,
+  eventLocation TEXT,
+  fatalities INTEGER,
+  injuries INTEGER,
+  weather TEXT,
+  lighting TEXT,
+  incidentType TEXT,
+  surfaceCondition TEXT,
+  trafficControl TEXT,
+  vehicleType TEXT,
+  vehicleSpecification TEXT,
+  vehicleDirection TEXT,
+  vehicleManeuver TEXT
+
 )
